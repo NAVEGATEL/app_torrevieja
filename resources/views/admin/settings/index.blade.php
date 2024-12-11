5cd9e76d8d22e5d41422010a2bc7fdb1455ffd60
EdvardIA <edvardks97@icloud.com>
2024-12-11 11:25:03 +0100

    code: En Ajustes se guardan e imprimen los datos

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -4,6 +4,10 @@
 <div class="text-center" style="background-color:#b8bbac; height:90vh;">
 
     <img src="https://c.tenor.com/OTvGTrjTMaYAAAAC/tenor.gif" alt="" style="margin-top:10%;">
+
+
 </div>
+<!-- Importamos el archivo de JavaScript usando Vite -->
+<script src="{{ mix('resources\js\admin\obtencionDatos.js') }}"></script>
 
 @endsection
